3cd2c1af22e728c83fa55ce7963d68c4c630f6e4
cl-gavan <151669055+cl-gavan@users.noreply.github.com>
2025-11-13 11:59:52 -0800

    Update launch_app.py
    Make this a runnable python file

--- cml/launch_app.py
+++ cml/launch_app.py
@@ -37,5 +37,7 @@
 #  DATA.
 #
 # ###########################################################################
-
-streamlit run app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
+import subprocess
+import os
+port = os.environ.get('CDSW_APP_PORT', '8080')
+subprocess.run(['streamlit', 'run', 'app.py', '--server.port', port, '--server.address', '127.0.0.1'])
